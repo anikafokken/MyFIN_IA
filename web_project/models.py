7e199c6a70a0e2822ff31b71e0f30cd1e2f4f7ab
Fokken <s813364@ISD625.SPPSMN.INT>
2026-02-02 13:01:01 -0600

    drafted most classes setup

--- web_project/models.py
+++ web_project/models.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from django.db import models
 
 class Student(models.Model):
@@ -17,7 +18,7 @@ class Student(models.Model):
         return self.title
     
 class School(models.Model):
-    # is_private
+    is_private = False
     program_list = list()
     # location
 
@@ -28,9 +29,50 @@ class School(models.Model):
     def add_program(program):
         print(program)
 
-# class Program(models.Model):
-    
+class Program(models.Model):
+    id = 0
+    approval_status = False
+    matched_students = list()
+    school = School()
+    # program_profile
+
+class ProgramProfile(models.Model):
+    # degree
+    # schedule
+    tuition = 0.0
+    # dorm_requirement
+    # dorm_availability
+    acceptance_rate = 0.0
+    esl_support = False
+    school = School()
+
+class Degree(models.Model):
+    # level
+    # specialty
+    # format
+    id = 0
+
+class DegreeLevel(Enum):
+    PRACTICAL_NURSING = 1
+    ASN = 2
+    BSN = 3
+    BSN_PRELIC = 4
+    GRAD_CERT = 5
+    MSN = 6
+    MSN_PRELIC = 7
+    DNP = 8
+    PHD = 9
 
 
+class ScheduleType(Enum):
+    PART_TIME = 1
+    FULL_TIME = 2
 
-# class ProgramProfile(models.Model
+class DormRequirement(Enum):
+    REQUIRED = 1
+    OPTIONAL = 2
+
+class DormAvailability(Enum):
+    AVAILABLE = 1
+    LIMITED = 2
+    UNAVAILABLE = 3
