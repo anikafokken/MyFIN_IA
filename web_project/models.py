769ffba43762060e7c4512569581aadb3419d48f
Fokken <s813364@ISD625.SPPSMN.int>
2026-02-02 12:38:21 -0600

    clean up errors

--- web_project/models.py
+++ web_project/models.py
@@ -2,22 +2,35 @@ from django.db import models
 
 class Student(models.Model):
     id = 1
-    name = "Anika Fokken"
+    name = ""
     # desired_program_types
     # constraints
     # preferences
     # matched_list
     # confirmed_matches
 
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
     def __str__(self):
         return self.title
     
 class School(models.Model):
+    # is_private
+    program_list = list()
+    # location
 
+    def __init__(self, id, program_list=list()):
+        self.program_list = program_list
+        self.id = id
 
-class Program(models.Model):
+    def add_program(program):
+        print(program)
+
+# class Program(models.Model):
     
 
 
 
-class ProgramProfile(models.Model)
+# class ProgramProfile(models.Model
